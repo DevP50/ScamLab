39d5d1547e2b302f16a34e6bef98ea3479301ce4
nana <nanagilbertjunior@gmail.com>
2026-08-29 13:58:56 +0100

    Feat: Defined the scoring logic and added difficulty based scoring

--- app/services/calculate_score.py
+++ app/services/calculate_score.py
@@ -13,12 +13,12 @@ def calculate_score(correct_answer,difficulty,scenario_clues,selected_clues):
            else:
             wrong_clue_points += 1
             wrong_clue_xp = wrong_clue_points * 5
-            if difficulty == "EASY":
-               difficulty_points += 0
-            elif difficulty == "MEDIUM":
-               difficulty_points += 5
-            elif difficulty == "HARD":
-               difficulty_points += 10
+    if difficulty == "EASY":
+         difficulty_points += 0
+    elif difficulty == "MEDIUM":
+         difficulty_points += 5
+    elif difficulty == "HARD":
+      difficulty_points += 10
     total_score = clue_xp + answer_xp - wrong_clue_xp + difficulty_points
 
     return {
