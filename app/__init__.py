c1fb8b58fb69fb7066c9f9072fe021e786d8dd07
nana <nanagilbertjunior@gmail.com>
2026-08-30 23:33:05 +0100

    Initializing login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from extensions import db
+from extensions import db,login_manager
 import os
 from dotenv import load_dotenv
 from app.routes.routes import  auth_bp,app_bp
@@ -8,10 +8,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.secret_key = os.getenv("SECRET_KEY")
+    login_manager.init_app(app)
     db.init_app(app)
     with app.app_context():
         db.create_all()
-
+    
     app.register_blueprint(auth_bp, url_prefix="/")
     app.register_blueprint(app_bp,url_prefix="")
     return app
