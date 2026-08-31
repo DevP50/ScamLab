0c1d0abed72ad890cff938c3f3b0653917a48fc1
nana <nanagilbertjunior@gmail.com>
2026-08-31 17:25:34 +0100

    Calculates score and xp

--- app/services/calculate_score.py
+++ app/services/calculate_score.py
@@ -1,34 +1,62 @@
 from app.models import ScenarioDifficulty
-def calculate_score(correct_answer,difficulty,scenario_clues,selected_clues):
-    answer_xp = 0
-    clue_xp=0
-    wrong_clue_xp = 0
-    correct_clue_points = 0
-    wrong_clue_points = 0
-    difficulty_points = 0
-    if correct_answer:
-        answer_xp = 50
-        for clue in selected_clues:
-           if clue in scenario_clues:
-            correct_clue_points += 1
-            clue_xp = correct_clue_points * 10
-           else:
-            wrong_clue_points += 1
-            wrong_clue_xp = wrong_clue_points * 5
+
+
+def calculate_score(
+    correct_answer,
+    difficulty,
+    scenario_clues,
+    selected_clues
+):
+
+    if not correct_answer:
+        return {
+            "total_score": 0,
+            "answer_xp": 0,
+            "clue_xp": 0,
+            "wrong_clue_xp": 0,
+            "difficulty_points": 0
+        }
+
+    answer_points = 50
+
+    correct_clues = sum(
+        1
+        for clue in selected_clues
+        if clue in scenario_clues
+    )
+
+    wrong_clues = sum(
+        1
+        for clue in selected_clues
+        if clue not in scenario_clues
+    )
+
+    clue_points = correct_clues * 10
+    wrong_clue_penalty = wrong_clues * 5
+
     if difficulty == ScenarioDifficulty.EASY:
-         difficulty_points += 0
+        difficulty_points = 0
+
     elif difficulty == ScenarioDifficulty.MEDIUM:
-         difficulty_points += 5
+        difficulty_points = 5
+
     elif difficulty == ScenarioDifficulty.HARD:
-      difficulty_points += 10
-    total_score = clue_xp + answer_xp - wrong_clue_xp + difficulty_points
+        difficulty_points = 10
+
+    else:
+        difficulty_points = 0
+
+    total_score = (
+        answer_points
+        + clue_points
+        - wrong_clue_penalty
+        + difficulty_points
+    )
 
     return {
-       "total_score": total_score,
-       "answer_xp": answer_xp,
-       "clue_xp": clue_xp,
-       "wrong_clue_xp": wrong_clue_xp,
-       "difficulty_points": difficulty_points
+        "total_score": total_score,
+        "answer_xp": answer_points,
+        "clue_xp": clue_points,
+        "wrong_clue_xp": wrong_clue_penalty,
+        "difficulty_points": difficulty_points
     }
-
-    
--- app/services/calculate_xp.py
+++ app/services/calculate_xp.py
@@ -1,23 +1,19 @@
-def calculate_xp(correct_answer,selected_clues,scenario_clues):
-     answer_xp = 0
-     clue_xp = 0
-     if correct_answer:
-         answer_xp = 50 
-         wrong_selected_clues = 0
-         correct_selected_clues = 0
+def calculate_xp(correct_answer, selected_clues, scenario_clues):
 
-         for clue in selected_clues:
-           if clue in scenario_clues:
-            correct_selected_clues += 1
-           else:
-              wrong_selected_clues += 1
-         clue_xp = correct_selected_clues * 10
+    answer_xp = 50 if correct_answer else 0
 
-     total_xp = answer_xp + clue_xp
+    correct_clues = sum(
+        1
+        for clue in selected_clues
+        if clue in scenario_clues
+    )
 
-     return {
+    clue_xp = correct_clues * 10 if correct_answer else 0
+
+    total_xp = answer_xp + clue_xp
+
+    return {
         "answer_xp": answer_xp,
         "clue_xp": clue_xp,
         "total_xp": total_xp
     }
-
