50aaea37aa9b50e9cca42d610e92b26b3760f67f
nana <nanagilbertjunior@gmail.com>
2026-08-31 17:27:23 +0100

    Selects scenarios based on user's weaknesses

--- app/services/scenario_selector.py
+++ app/services/scenario_selector.py
@@ -1,13 +1,26 @@
-from app.models import Scenario, Attempt
+from app.models import Scenario, Attempt, ScenarioDifficulty
+
+
 def select_scenario(category, difficulty, user_id):
+
     attempts = Attempt.query.filter_by(
         user_id=user_id
     ).all()
 
-    attempted_ids = [attempt.scenario_id for attempt in attempts]
-    scenarios = Scenario.query.filter(#Used for sqlalchemy expressions like equality etc while filter_by is used for keyword matching
-    Scenario.category == category,
-    Scenario.difficulty == difficulty,
-    Scenario.id.not_in(attempted_ids)
-).all()
-    return scenarios
+    attempted_ids = [
+        attempt.scenario_id
+        for attempt in attempts
+    ]
+
+    difficulty_enum = ScenarioDifficulty(difficulty)#Convert the string difficulty into an enum 
+
+    scenarios = Scenario.query.filter(
+        Scenario.category == category,
+        Scenario.difficulty == difficulty_enum,
+        ~Scenario.id.in_(attempted_ids)
+    ).all()
+
+    if not scenarios:
+        return None
+
+    return scenarios[0]
